6c68067b61546e6b88a9242ed9c4e80bb8e4288d
ruslannurtaev <126150314+ruslannurtaev@users.noreply.github.com>
2023-03-20 21:55:38 +0300

    Update prog.s
    with -O3 (optimization)

--- prog.s
+++ prog.s
@@ -1,71 +1,49 @@
 	.file	"prog.c"
 	.text
-	.def	printf;	.scl	3;	.type	32;	.endef
-	.seh_proc	printf
-printf:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	pushq	%rbx
-	.seh_pushreg	%rbx
-	subq	$56, %rsp
-	.seh_stackalloc	56
-	leaq	48(%rsp), %rbp
-	.seh_setframe	%rbp, 48
-	.seh_endprologue
-	movq	%rcx, 32(%rbp)
-	movq	%rdx, 40(%rbp)
-	movq	%r8, 48(%rbp)
-	movq	%r9, 56(%rbp)
-	leaq	40(%rbp), %rax
-	movq	%rax, -16(%rbp)
-	movq	-16(%rbp), %rbx
-	movl	$1, %ecx
-	movq	__imp___acrt_iob_func(%rip), %rax
-	call	*%rax
-	movq	%rax, %rcx
-	movq	32(%rbp), %rax
-	movq	%rbx, %r8
-	movq	%rax, %rdx
-	call	__mingw_vfprintf
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	addq	$56, %rsp
-	popq	%rbx
-	popq	%rbp
-	ret
-	.seh_endproc
-	.def	__main;	.scl	2;	.type	32;	.endef
 	.section .rdata,"dr"
 .LC0:
 	.ascii "%d\0"
 	.text
+	.p2align 4
+	.def	printf.constprop.0;	.scl	3;	.type	32;	.endef
+	.seh_proc	printf.constprop.0
+printf.constprop.0:
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	movl	$1, %ecx
+	leaq	72(%rsp), %rbx
+	movq	%rdx, 72(%rsp)
+	movq	%r8, 80(%rsp)
+	movq	%r9, 88(%rsp)
+	movq	%rbx, 40(%rsp)
+	call	*__imp___acrt_iob_func(%rip)
+	movq	%rbx, %r8
+	leaq	.LC0(%rip), %rdx
+	movq	%rax, %rcx
+	call	__mingw_vfprintf
+	addq	$48, %rsp
+	popq	%rbx
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section	.text.startup,"x"
+	.p2align 4
 	.globl	main
 	.def	main;	.scl	2;	.type	32;	.endef
 	.seh_proc	main
 main:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$48, %rsp
-	.seh_stackalloc	48
+	subq	$40, %rsp
+	.seh_stackalloc	40
 	.seh_endprologue
 	call	__main
-	movl	$10, -4(%rbp)
-	movl	$50, -8(%rbp)
-	movl	$40, -12(%rbp)
-	movl	-4(%rbp), %edx
-	movl	-8(%rbp), %eax
-	addl	%eax, %edx
-	movl	-12(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, %edx
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rcx
-	call	printf
-	movl	$0, %eax
-	addq	$48, %rsp
-	popq	%rbp
+	movl	$100, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf.constprop.0
+	xorl	%eax, %eax
+	addq	$40, %rsp
 	ret
 	.seh_endproc
 	.ident	"GCC: (GNU) 12.1.0"
